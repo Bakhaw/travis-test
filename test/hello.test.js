a23ceb26150190390c3194f5fe56e69156ee5594
Nicolas MARTINHO <nicolas.martinho@outlook.fr>
2017-12-08 10:28:41 +0100

    first commit

---
+++ test/hello.test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+
+const hello = require('../hello.js');
+
+describe('Hello world', () => {
+  it("renvoie hello world", () => {
+    expect(hello()).to.equal('hello world')
+  })
+})
