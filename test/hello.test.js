affd70be2c80a22c1a9889c5de923447a9839d98
Faïssal <faissal.hattou@gmail.com>
2017-12-08 11:09:16 +0100

    test 1 passé (no triche)

--- test/hello.test.js
+++ test/hello.test.js
@@ -5,7 +5,7 @@ const expect = chai.expect;
 const hello = require('../hello.js');
 
 describe('Hello world', () => {
-  it("renvoie hello world", () => {
-    expect(hello()).to.equal('hello world')
-  })
-})
+  it('renvoie hello world', () => {
+    expect(hello()).to.equal('hello world');
+  });
+});
